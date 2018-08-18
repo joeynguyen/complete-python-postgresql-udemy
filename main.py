35bc452d028cebc5e1323c890d6ea49e12c66a06
Joey Nguyen <jnn193@gmail.com>
2018-08-18 18:54:42 -0500

    Get user input. Convert string to number

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-age = 5
-print(age)
-print("Joey was here")
+# age = 5
+age = input('Enter your age: ')
+print('You are ' + age + ' years old')
+ageInDays = int(age) * 365
+print('You are ' + str(ageInDays) + ' days old')
