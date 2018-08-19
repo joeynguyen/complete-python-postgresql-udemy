3e5ebf2bbd92498e0beb074749a68b499cabf20c
Joey Nguyen <jnn193@gmail.com>
2018-08-18 19:22:20 -0500

    for loops

--- magic_numbers.py
+++ magic_numbers.py
@@ -2,3 +2,6 @@ numbers = [0, 1, 2 ,3, 4]
 print('numbers:', numbers)
 print('numbers length:', len(numbers))
 print('last number:', numbers[len(numbers) - 1])
+
+for number in numbers:
+    print('{} to the power of 2 = {}'.format(str(number), str(number ** 2)))
