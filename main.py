bba852152615071265cf88849f05a12869c771a2
Joey Nguyen <jnn193@gmail.com>
2018-08-18 19:01:46 -0500

    Learned .format method

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ age = input('Enter your age: ')
 print('You are ' + age + ' years old')
 ageInDays = int(age) * 365
 print('You are ' + str(ageInDays) + ' days old')
+# format method implicitly converts number to string
+print('You are {} days old! This is equivalent to {} years.'.format(ageInDays, age))
