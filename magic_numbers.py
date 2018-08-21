5c977173a4fe0594844d5019704ec8d580c21653
Joey Nguyen <jnn193@gmail.com>
2018-08-21 01:41:53 -0500

    import random

--- magic_numbers.py
+++ magic_numbers.py
@@ -1,3 +1,5 @@
+import random
+
 numbers = [0, 1, 2 ,3, 4]
 print('numbers:', numbers)
 print('numbers length:', len(numbers))
@@ -6,7 +8,11 @@ print('last number:', numbers[len(numbers) - 1])
 for number in numbers:
     print('{} to the power of 2 = {}'.format(str(number), str(number ** 2)))
 
-magic_numbers = [3, 5, 9]
+magic_numbers = [
+    random.randint(0,9),
+    random.randint(0,9),
+    random.randint(0,9)
+]
 chances = 3
 guess_correct = False
 
@@ -19,3 +25,12 @@ while chances > 0 and guess_correct == False:
         print('You guessed wrong')
         chances -= 1
         print('You have {} chances left'.format(chances))
+
+minimum = 100
+for index in range(10):
+    random_number = random.randint(0, 100)
+    print('current number is {}'.format(random_number))
+    if random_number < minimum:
+        minimum = random_number
+
+print('minimum is {}'.format(minimum))
