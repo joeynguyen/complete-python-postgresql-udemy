fe711d3711db903a467186390dd436a16d5c42f9
Joey Nguyen <jnn193@gmail.com>
2018-08-29 00:20:34 -0500

    Dictionaries in depth

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,10 @@
-sample_dict = {'name': 'Jose', 'score': 25}
+sample_dict = {'name': 'Jose',
+               'score': [25, 50, 73, 98],
+               'exams': {
+                   'final': 80,
+                   'midterm': 92
+               }}
 
 print('name:', sample_dict['name'])
-print('score:', sample_dict['score'])
+print('score:', sample_dict['score'][3])
+print('score:', sample_dict['exams']['midterm'])
