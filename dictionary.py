a3117186be86d1b065c6dbbc48b5019674b936b2
Joey Nguyen <jnn193@gmail.com>
2018-08-30 01:46:58 -0500

    Return a dictionary

--- dictionary.py
+++ dictionary.py
@@ -8,3 +8,12 @@ sample_dict = {'name': 'Jose',
 print('name:', sample_dict['name'])
 print('score:', sample_dict['score'][3])
 print('score:', sample_dict['exams']['midterm'])
+
+def create_user():
+    user_name = input("What is your name? ")
+    user_age = input("What is your age? ")
+    return {'name': user_name,
+            'age': user_age}
+
+user_object = create_user()
+print("Your name is {} and you are {} years old.".format(user_object['name'], user_object['age']))
