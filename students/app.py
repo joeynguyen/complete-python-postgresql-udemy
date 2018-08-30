18ac46c9c828934278f3c0e08272495c25d5e6d0
Joey Nguyen <jnn193@gmail.com>
2018-08-30 02:17:26 -0500

    Create average method

--- students/app.py
+++ students/app.py
@@ -1,16 +1,27 @@
-def create_student():
+def create_student(grades):
     user_name = input("What is your name? ")
     return {'name': user_name,
-            'grades': []}
+            'grades': grades}
 
 def add_grade(student, grade):
     student['grades'].append(grade)
 
-new_student = create_student()
+def calculate_avg_grade(student):
+    total = sum(student['grades'])
+    count = len(student['grades'])
+    if count == 0:
+        return 0
+    return total / count
 
-new_student['grades'].append(5)
-print(new_student)
+new_student1 = create_student([])
 
-add_grade(new_student, 90)
-print(new_student)
+new_student1['grades'].append(5)
+print(new_student1)
 
+add_grade(new_student1, 90)
+print(new_student1)
+
+new_student2 = create_student([80, 70, 60])
+new_student3 = create_student([85, 95, 75])
+print("new_student2 avg: ", calculate_avg_grade(new_student2))
+print("new_student3 avg: ", calculate_avg_grade(new_student3))
