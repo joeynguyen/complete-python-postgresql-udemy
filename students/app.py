2d1ab72c27af5cc7bd7a8cc5286f1fe010171d5a
Joey Nguyen <jnn193@gmail.com>
2018-08-30 02:20:45 -0500

    Refactor and test for 0 count

--- students/app.py
+++ students/app.py
@@ -7,10 +7,11 @@ def add_grade(student, grade):
     student['grades'].append(grade)
 
 def calculate_avg_grade(student):
-    total = sum(student['grades'])
     count = len(student['grades'])
     if count == 0:
         return 0
+
+    total = sum(student['grades'])
     return total / count
 
 new_student1 = create_student([])
@@ -23,5 +24,7 @@ print(new_student1)
 
 new_student2 = create_student([80, 70, 60])
 new_student3 = create_student([85, 95, 75])
+new_student4 = create_student([])
 print("new_student2 avg: ", calculate_avg_grade(new_student2))
 print("new_student3 avg: ", calculate_avg_grade(new_student3))
+print("new_student4 avg: ", calculate_avg_grade(new_student4))
