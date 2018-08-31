add6dce836200ec78c892eb85d0420b66c4a5d11
Joey Nguyen <jnn193@gmail.com>
2018-08-31 17:25:19 -0500

    print details of students list

--- students/app.py
+++ students/app.py
@@ -1,5 +1,5 @@
-def create_student(grades):
-    user_name = input("What is your name? ")
+def create_student(name, grades):
+    user_name = name
     return {'name': user_name,
             'grades': grades}
 
@@ -14,7 +14,7 @@ def calculate_avg_grade(student):
     total = sum(student['grades'])
     return total / count
 
-new_student1 = create_student([])
+new_student1 = create_student('Tim', [])
 
 new_student1['grades'].append(5)
 print(new_student1)
@@ -22,16 +22,19 @@ print(new_student1)
 add_grade(new_student1, 90)
 print(new_student1)
 
-new_student2 = create_student([80, 70, 60])
-new_student3 = create_student([85, 95, 75])
-new_student4 = create_student([])
+new_student2 = create_student('Joe', [80, 70, 60])
+new_student3 = create_student('Bill', [85, 95, 75])
+new_student4 = create_student('Bob', [])
 
-def student_details(student):
+student_list = []
+student_list.extend([new_student1, new_student2, new_student3, new_student4])
+print(student_list)
+
+def print_student_details(student):
     print("{}, average grade: {}.".format(student['name'],
                                           calculate_avg_grade(student)
                                          )
          )
 
-student_details(new_student2)
-student_details(new_student3)
-student_details(new_student4)
+for student in student_list:
+    print_student_details(student)
