916f578a23d209cdf6ffd73295ea72e6a96ddb19
Joey Nguyen <jnn193@gmail.com>
2018-08-31 17:14:36 -0500

    Method to print student details

--- students/app.py
+++ students/app.py
@@ -25,6 +25,13 @@ print(new_student1)
 new_student2 = create_student([80, 70, 60])
 new_student3 = create_student([85, 95, 75])
 new_student4 = create_student([])
-print("new_student2 avg: ", calculate_avg_grade(new_student2))
-print("new_student3 avg: ", calculate_avg_grade(new_student3))
-print("new_student4 avg: ", calculate_avg_grade(new_student4))
+
+def student_details(student):
+    print("{}, average grade: {}.".format(student['name'],
+                                          calculate_avg_grade(student)
+                                         )
+         )
+
+student_details(new_student2)
+student_details(new_student3)
+student_details(new_student4)
