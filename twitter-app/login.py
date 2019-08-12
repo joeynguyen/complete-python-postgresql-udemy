9607909961b9f56b475e4278bac17c27a094af37
Joey Nguyen <jnn193@gmail.com>
2019-08-11 22:50:34 -0500

    Update app to retrieve access token from Twitter API

--- twitter-app/login.py
+++ twitter-app/login.py
@@ -8,6 +8,22 @@ client = oauth2.Client(consumer)
 
 response, content = client.request(constants.REQUEST_TOKEN_URL, 'POST')
 if response.status != 200:
-    print('An error occurred getting the request token from Twitter')
+    print("An error occurred getting the request token from Twitter")
 
 request_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+
+print("Go to the following site in your browser:")
+print("{}?oauth_token={}".format(constants.AUTHORIZATION_URL, request_token['oauth_token']))
+# print("{}?oauth_token={}".format(constants.AUTHORIZATION_URL, request_token.oauth_token))
+
+token = oauth2.Token(request_token['oauth_token'], request_token['oauth_token_secret'])
+
+oauth_verifier = input("What is the PIN? ")
+
+token.set_verifier(oauth_verifier)
+client = oauth2.Client(consumer, token)
+
+response, content = client.request(constants.ACCESS_TOKEN_URL, 'POST')
+access_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
+
+print('access_token', access_token)
