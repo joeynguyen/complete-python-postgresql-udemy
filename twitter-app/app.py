0e7f97cd511d546c4aa429bf0f97d7e9b33ba0dd
Joey Nguyen <jnn193@gmail.com>
2019-09-13 23:58:27 -0500

    Add global 'user' variable. Redirect user to profile page if already logged in

--- twitter-app/app.py
+++ twitter-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, request, url_for
+from flask import Flask, render_template, session, redirect, request, url_for, g
 
 from database import Database
 from user import User
@@ -10,6 +10,12 @@ app.secret_key = '1234'
 Database.initialise(user='joeynguyen', password='password', host='localhost', database='joeynguyen')
 
 
+@app.before_request
+def load_user():
+    if 'screen_name' in session:
+        g.user = User.load_from_db(session['screen_name'])
+
+
 @app.route('/')
 def homepage():
     return render_template('home.html')
@@ -17,13 +23,20 @@ def homepage():
 
 @app.route('/login/twitter')
 def twitter_login():
+    if 'screen_name' in session:
+        return redirect(url_for('profile'))
+
     request_token = get_request_token()
     session['request_token'] = request_token
-    print('request_token', request_token)
-    print('session', session)
 
     # redirect the user
     return redirect(get_oauth_verifier_url(request_token))
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('homepage'))
 
 
 @app.route('/auth/twitter')  # http://127.0.0.1:4995/auth/twitter?oauth_token=xwA_bBPdHZ0&oauth_verifier=MdRcq5y
@@ -46,7 +59,7 @@ def twitter_auth():
 
 @app.route('/profile')
 def profile():
-    return render_template('profile.html', screen_name=session['screen_name'])
+    return render_template('profile.html', user=g.user)
 
 
 app.run(port=4995, debug=True)
